f87a4fecf8146ced9139487119f5ae9d3a097a37
Pankaj Manral <pankaj_manral@ildc-inlp.idealake.com>
2025-06-16 13:22:43 +0530

    added footer section

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,10 +5,10 @@ hamburger.addEventListener('click', function(){
     this.classList.toggle('open')
     
     if(hamburger.classList.contains('open')){
-        mobileNav.style.opacity = '1'
-        mobileNav.style.top = '90px'
+        mobileNav.style.transform = 'translateX(0%)'
+        document.querySelector('body').style.overflow = 'hidden'
     }else{
-        mobileNav.style.opacity = '0'
-        mobileNav.style.top = '0px'
+        mobileNav.style.transform = 'translateX(-100%)'
+        document.querySelector('body').style.overflow = 'auto'
     }
 })
